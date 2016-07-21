9745041f3b29330b850415c9df50cb175d372eb4
Geoff Blair <gmblair@gmail.com>
2016-07-21 07:30:28 -0700

    Add Vector2.dot and Vector2.angleBetween

--- Vector2.js
+++ Vector2.js
@@ -46,6 +46,14 @@ proto.normalize = function () {
   }
 };
 
+proto.dot = function (v) {
+  return exports.dot(this.x, this.y, v.x, v.y);
+};
+
+proto.angleBetween = function (v) {
+  return exports.angleBetween(this.x, this.y, v.x, v.y);
+};
+
 Object.defineProperty(proto, "magnitude", {
   get: function () {
     return exports.magnitude(this.x, this.y);
@@ -64,4 +72,15 @@ exports.magnitude = function (x, y) {
 
 exports.angle = function (x, y) {
   return Math.atan2(y, x);
+};
+
+exports.dot = function (x1, y1, x2, y2) {
+  return x1 * x2 + y1 * y2;
+};
+
+exports.angleBetween = function (x1, y1, x2, y2) {
+  var dot = exports.dot(x1, y1, x2, y2);
+  var m1 = exports.magnitude(x1, y1);
+  var m2 = exports.magnitude(x2, y2);
+  return Math.acos(dot / (m1 * m2));
 };
