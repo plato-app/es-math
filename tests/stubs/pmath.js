191eece8865053e7c56fe243a0b81c45e3d56a75
Geoff Blair <gmblair@gmail.com>
2017-03-25 16:58:40 -0700

    Don't lint tests

--- tests/stubs/pmath.js
+++ tests/stubs/pmath.js
@@ -2,7 +2,7 @@
 
 exports.acos = Math.acos;
 exports.asin = Math.asin;
-exports.atan2 = Math.atan2
+exports.atan2 = Math.atan2;
 exports.cos = Math.cos;
 exports.pow = Math.pow;
 exports.sin = Math.sin;
