60f8548e096cb00fc0d7d195c993c653b06dcfed
Geoff Blair <gmblair@gmail.com>
2018-09-24 19:24:41 -0600

    :shirt: Remove redundant type comments in JSDoc

--- lib/index.ts
+++ lib/index.ts
@@ -20,17 +20,16 @@ export interface IRectangle {
 
 /**
  * TAU is a handy shortcut for 2PI
- * @type {number}
  * @since 1.2.1
  */
 export const TAU = Math.PI * 2;
 
 /**
  * Compares two numbers for approximate equality
- * @param {number} a Number A
- * @param {number} b Number B
- * @param {number} threshold Equality threshold (defaults to Number.EPSILON)
- * @returns {boolean} Whether a is approximately equal to b
+ * @param a Number A
+ * @param b Number B
+ * @param threshold Equality threshold (defaults to Number.EPSILON)
+ * @returns Whether a is approximately equal to b
  * @since 1.2.1
  */
 export function approximately(a: number, b: number, threshold = Number.EPSILON): boolean {
@@ -39,6 +38,8 @@ export function approximately(a: number, b: number, threshold = Number.EPSILON):
 
 /**
  * Returns the average of an array of numbers
+ * @param numbers Numbers to average
+ * @returns Average of given numbers
  * @since 3.1.0
  */
 export function average(numbers: number[]): number {
@@ -48,10 +49,10 @@ export function average(numbers: number[]): number {
 
 /**
  * Clamps a number between a minimum and maximum
- * @param {number} n Number to be clamped
- * @param {number} min Minimum
- * @param {number} max Maximum
- * @returns {number} Clamped value of n
+ * @param n Number to be clamped
+ * @param min Minimum
+ * @param max Maximum
+ * @returns Clamped value of n
  * @since 1.2.1
  */
 export function clamp(n: number, min: number, max: number): number {
@@ -60,8 +61,8 @@ export function clamp(n: number, min: number, max: number): number {
 
 /**
  * Converts degrees to radians
- * @param {number} degrees Angle in degrees
- * @returns {number} Angle in radians
+ * @param degrees Angle in degrees
+ * @returns Angle in radians
  * @since 3.3.0
  */
 export function degreesToRadians(degrees: number): number {
@@ -70,11 +71,11 @@ export function degreesToRadians(degrees: number): number {
 
 /**
  * Calculates the distance between two points
- * @param {number} x1 First X coordinate
- * @param {number} y1 First Y coordinate
- * @param {number} x2 Second X coordinate
- * @param {number} y2 Second Y coordinate
- * @returns {number} Distance between x1, y1 and x2, y2
+ * @param x1 First X coordinate
+ * @param y1 First Y coordinate
+ * @param x2 Second X coordinate
+ * @param y2 Second Y coordinate
+ * @returns Distance between x1, y1 and x2, y2
  * @since 1.2.1
  */
 export function distance(x1: number, y1: number, x2: number, y2: number): number {
@@ -83,9 +84,9 @@ export function distance(x1: number, y1: number, x2: number, y2: number): number
 
 /**
  * Converts an index to grid X coordinate
- * @param {number} index Index
- * @param {number} width Grid width
- * @returns {number} X coordinate for index
+ * @param index Index
+ * @param width Grid width
+ * @returns X coordinate for index
  * @since 1.2.1
  */
 export function indexToX(index: number, width: number): number {
@@ -94,9 +95,9 @@ export function indexToX(index: number, width: number): number {
 
 /**
  * Converts an index to grid Y coordinate
- * @param {number} index Index
- * @param {number} width Grid width
- * @returns {number} Y coordinate for index
+ * @param index Index
+ * @param width Grid width
+ * @returns Y coordinate for index
  * @since 1.2.1
  */
 export function indexToY(index: number, width: number): number {
@@ -105,11 +106,11 @@ export function indexToY(index: number, width: number): number {
 
 /**
  * Linear interpolation between two numbers
- * @param {number} a Number A
- * @param {number} b Number B
- * @param {number} t Normalized time (between 0 - 1)
- * @param {boolean} roundResult Rounds return value
- * @returns {number} Interpolated value between a and b
+ * @param a Number A
+ * @param b Number B
+ * @param t Normalized time (between 0 - 1)
+ * @param roundResult Rounds return value
+ * @returns Interpolated value between a and b
  * @since 1.2.1
  */
 export function lerp(a: number, b: number, t: number, roundResult = false): number {
@@ -119,6 +120,10 @@ export function lerp(a: number, b: number, t: number, roundResult = false): numb
 
 /**
  * Linear interpolation between two angles via shortest angle
+ * @param a Start angle
+ * @param b End angle
+ * @param t Normalized time (between 0 - 1)
+ * @returns Interpolated angle between a and b
  * @since 3.2.0
  */
 export function lerpAngle(a: number, b: number, t: number): number {
@@ -129,9 +134,9 @@ export function lerpAngle(a: number, b: number, t: number): number {
 
 /**
  * Returns the mid value between two numbers
- * @param {number} a Number A
- * @param {number} b Number B
- * @returns {number} Mid value between a and b
+ * @param a Number A
+ * @param b Number B
+ * @returns Mid value between a and b
  * @since 1.2.1
  */
 export function mid(a: number, b: number): number {
@@ -140,10 +145,10 @@ export function mid(a: number, b: number): number {
 
 /**
  * Normalizes a value between a minimum and maximum
- * @param {number} n Value to normalize
- * @param {number} min Minimum
- * @param {number} max Maximum
- * @returns {number} Normalized value of n, between min and max
+ * @param n Value to normalize
+ * @param min Minimum
+ * @param max Maximum
+ * @returns Normalized value of n, between min and max
  * @since 1.2.1
  */
 export function normalize(n: number, min: number, max: number): number {
@@ -152,8 +157,8 @@ export function normalize(n: number, min: number, max: number): number {
 
 /**
  * Converts radians to degrees
- * @param {number} radians Radians
- * @returns {number} Degrees
+ * @param radians Radians
+ * @returns Degrees
  * @since 3.4.0
  */
 export function radiansToDegrees(radians: number): number {
@@ -162,9 +167,9 @@ export function radiansToDegrees(radians: number): number {
 
 /**
  * Rounds a value to a specified number of decimal places
- * @param {number} value Value to round
- * @param {number} decimals Number of decimal places to round
- * @returns {number} Rounded value of value
+ * @param value Value to round
+ * @param decimals Number of decimal places to round
+ * @returns Rounded value of value
  * @since 1.2.1
  */
 export function round(value: number, decimals = 0): number {
@@ -174,8 +179,8 @@ export function round(value: number, decimals = 0): number {
 
 /**
  * Returns the sign of a number
- * @param {number} n Number
- * @returns {number} Sign of n, -1, 0, or 1
+ * @param n Number
+ * @returns Sign of n, -1, 0, or 1
  * @since 1.2.1
  */
 export function sign(n: number): number {
@@ -184,10 +189,10 @@ export function sign(n: number): number {
 
 /**
  * Converts grid coordinates to an index
- * @param {number} x X coordinate
- * @param {number} y Y coordinate
- * @param {number} width Grid width
- * @returns {number} Index value of x, y
+ * @param x X coordinate
+ * @param y Y coordinate
+ * @param width Grid width
+ * @returns Index value of x, y
  * @since 1.2.1
  */
 export function xyToIndex(x: number, y: number, width: number): number {
